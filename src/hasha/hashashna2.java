1471c1847e9afeec4612f230da64221d910fb79b
Zaouk <khaled.zaouk@telecom-paristech.fr>
2016-02-11 20:32:26 +0100

    bla

--- src/hasha/hashashna2.java
+++ src/hasha/hashashna2.java
@@ -3,11 +3,15 @@ package hasha;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class hashashna2 {
 	Integer Max;
 	HashMap<Integer,ProductType> prods;
+	ArrayList<Warehouse> warehouses;
+	HashMap<ProductType,Integer> items;
+	int nbProds;
 
 	public void parseHeader(String line)
 	{
@@ -16,7 +20,8 @@ public class hashashna2 {
 	
 	public void readProductTypes(BufferedReader br)
 	{
-		int nbProds;
+		nbProds=0;
+		
 		String prodWeights;
 		String[] weights;
 		try {
@@ -39,7 +44,39 @@ public class hashashna2 {
 		
 	}
 	
-	public void 
+	public void readWareHouses(BufferedReader br)
+	{
+		int r,c;
+		String[] ll;
+		try {
+			
+			int nbwarehouse=Integer.parseInt(br.readLine());
+			for(int i=0;i<nbwarehouse;i++)
+			{
+			String loca=br.readLine();
+			ll=loca.split("\\s+");
+			r=Integer.parseInt(ll[0]);
+			c=Integer.parseInt(ll[1]);
+			HashMap<ProductType,Integer> curitems=new HashMap<>();
+			
+			String items=br.readLine();
+			String[] ss=items.split("\\s+");
+			for(int i=0i<nbProds;i++)
+			{
+				if(!ss[i].equals("0"))
+				{
+					//there are items of this prodtype
+					curitems.put(prods.get(i), Integer.parseInt(ss[i]));
+				}
+			}
+			warehouses.add(new Warehouse(new Point(r,c),curitems));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 	public void parseFile()
 	{	
