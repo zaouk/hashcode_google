6e7cfb6f1c713c28d60469aef074d53f87f6e09b
dandachi <mohamad-dandachi@hotmail.com>
2016-02-11 20:36:42 +0100

    sdqsdsq

--- src/hasha/Order.java
+++ src/hasha/Order.java
@@ -1,28 +1,49 @@
 package hasha;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Order {
-	ArrayList<ProductType> items;
+	HashMap<ProductType, Integer> items;
 	Point location;
-	
-	public Order()
-	{
-		
+
+	public Order(Point p) {
+		location = p;
 	}
-	
-	public ArrayList<ProductType> getItems() {
-		return items;
+
+	public void addItem(ProductType type, Integer nbitems) {
+		if (!items.containsKey(type))
+			items.put(type, nbitems);
+		else {
+			items.put(type, items.get(type)+nbitems);
+		}
 	}
-	public void setItems(ArrayList<ProductType> items) {
-		this.items = items;
+
+	public Boolean removeItems(ProductType type, Integer nbItems) {
+		Integer currentCount = items.get(type);
+		if (currentCount > nbItems) {
+			items.put(type, currentCount - nbItems);
+			return true;
+		} else {
+			if (currentCount == nbItems) {
+				items.remove(type);
+				return true;
+			} else
+				return false;
+		}
 	}
+
+	public Integer getCount(ProductType type) {
+		if (items.containsKey(type))
+			return items.get(type);
+		return 0;
+	}
+
 	public Point getLocation() {
 		return location;
 	}
+
 	public void setLocation(Point location) {
 		this.location = location;
 	}
-	
 
 }
--- src/hasha/hashashna.java
+++ src/hasha/hashashna.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class hashashna {
 	Integer rowsMax,columnsMax,numberOfDrones,deadlineTime,maxLoad;
 	ArrayList<Drone> drones;
 	Integer numberOrders;
 	ArrayList<Order> orders;
+	
+	HashMap<Integer,ProductType> prods;
+	
 	public void parseHeader(String line)
 	{
 		String[] splitline;
@@ -30,12 +34,29 @@ public class hashashna {
 	public void parseOrder(BufferedReader br) throws IOException
 	{
 		String line=br.readLine();
+		String[] splitted;
+		Integer r,c,L;
 		numberOrders=Integer.parseInt(line);
-		line=br.readLine();
 		orders=new ArrayList<Order>();
 		for(int i=0;i<numberOrders;i++)
 		{
+			line=br.readLine();
+			splitted=line.split("");
+			r=Integer.parseInt(splitted[0]);
+			c=Integer.parseInt(splitted[1]);
+			line=br.readLine();
+			L=Integer.parseInt(line);
+			Order order=new Order(new Point(r, c));
 			
+			
+			line=br.readLine();
+			splitted=line.split("");
+			
+			for(int j=0;j<L;j++)
+			{
+				order.addItem(prods.get(j), 1);
+			}
+			orders.add(order);
 		}
 	}
 	public void parseFile()
